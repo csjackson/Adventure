a58c43b8b55b9999fc055328c48b47d4d7c2e998
Christopher Jackson <csjackson@gmail.com>
2011-03-29 16:30:55 -0500

    Inventory has write out of scope

---
+++ src/Adventure.Tests/InventoryCommandTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using Adventure.Data;
+
+namespace Adventure.Tests
+{
+    [TestClass]
+    public class InventoryCommandTest
+    {
+        private IConsoleFacade mock;
+        private InventoryCommand cmd;
+        private IRepository<Item> repository;
+
+        [TestInitialize]
+        public void Before_Each_Test()
+        {
+            mock = MockRepository.GenerateMock<IConsoleFacade>();
+            repository = MockRepository.GenerateMock<IRepository<Item>>();
+            cmd = new InventoryCommand(mock, repository);
+        }
+
+        [TestMethod]
+        public void IsValid_Should_Return_False_for_Invalid_String()
+        {
+            // Arrange
+            // Already created via TestInitialize
+
+            // Act
+            var result = cmd.IsValid("Stupid stuff");
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void isValid_Should_Return_True_for_Valid_String()
+        {
+            // Arrange
+
+            // Act
+            var result = cmd.IsValid("inventory");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Execute_Should_List_Contents_Of_Inventory()
+        {
+            // Arrange
+
+            // Act
+            cmd.Execute("inventory");
+
+            // Assert
+            Assert.Fail();
+            repository.AssertWasCalled(m => m.Dispose());
+        }
+
+    }
+}
---
+++ src/Adventure/Commands/InventoryCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Adventure.Data;
+
+namespace Adventure.Commands
+{
+    public class InventoryCommand : BaseCommand, ICommand
+    {
+
+        private IConsoleFacade console;
+        private IRepository<Item> repository;
+
+        public InventoryCommand(IConsoleFacade console, IRepository<Item> repository)
+        {
+            this.console = console;
+            this.repository = repository;
+        }
+
+        public bool IsValid(string input)
+        {
+            return (IsFirstWord(input, "inventory"));
+        }
+
+        public void Execute(string input)
+        {
+            console.WriteLine("You are carrying:");
+            using (repository)
+            {
+                var sack = repository.AsQueryable().Where(qq => qq.RoomId == 2);
+                foreach (var item in sack)
+                {
+                    write("{0}  ", item.ItemName);
+                }
+               
+                /* for each item in repositroy where repository.RoomId=2
+                 * Writeline (item.ItemName);
+                 */
+            }
+
+        }
+    }
+}
