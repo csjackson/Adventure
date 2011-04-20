8c5daa68609a54348980a714d9f8ad723bb89150
Christopher Jackson <csjackson@gmail.com>
2011-04-19 21:32:02 -0500

    CreateExit first update

--- src/Adventure.Tests/CreateExitTest.cs
+++ src/Adventure.Tests/CreateExitTest.cs
@@ -3,15 +3,61 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using Adventure.Data;
+using Adventure.Commands;
 
 namespace Adventure.Tests
 {
     [TestClass]
-    public class CreateExitTest
+    public class CreateExitTest : BaseTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private CreateExitCommand cmd;
+        [TestInitialize]
+        public void Before_Every_Test()
         {
+            cmd = new CreateExitCommand();
+        }
+        [TestMethod]
+        public void IsValid_Should_Return_False_for_Invalid_String()
+        {
+            // Arrange
+            // Already created via TestInitialize
+
+            // Act
+            var result = cmd.IsValid("Stupid stuff");
+            // Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void isValid_Should_Return_True_for_Valid_String()
+        {
+            // Arrange
+
+            // Act
+            var result = cmd.IsValid("CreateExit Blah=Hallway");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void CreateExit_Should_Create_An_Exit()
+        {
+        	// Arrange
+            // Act
+            cmd.Execute("Createexit blah=Hallway");
+            // Assert
+            Assert.Fail();
+        }
+        [TestMethod]
+        public void CreateExit_Should_Check_For_Valid_Destination()
+        {
+            // Arrange
+
+            // Act 
+            cmd.Execute("Createexit blah=Foo");
+            // Assert
+            console.AssertWasCalled(qq => qq.WriteLine("Destination '{0}' was not found", "Foo"));
         }
     }
 }
--- src/Adventure/Commands/CreateExitCommand.cs
+++ src/Adventure/Commands/CreateExitCommand.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Adventure.Data;
 
 namespace Adventure.Commands
 {
-    class CreateExitCommand
+    public class CreateExitCommand : BaseCommand, ICommand
     {
+        private IConsoleFacade console;
+        private IRepository<GameObject> repository;
+        private IPlayer player;
+        private IRepository<ExitAlias> aliases;
+        public void CreateExitCommand(IConsoleFacade console, 
+            IRepository<GameObject> repository, IPlayer player, IRepository<ExitAlias> aliases)
+        {
+            this.console = console;
+            this.repository = repository;
+            this.player = player;
+            this.aliases = aliases;
+
+        }
+        public bool IsValid(string input)
+        {
+            return IsFirstWord(input, "createexit");
+        }
+        public void Execute(string input)
+        {
+            var output = GetAllButFirstWord(input);
+            string ExitName = output.TrimStart().Split('=')[0];
+            string Destination = output.TrimStart().Split('=')[1];
+        }
     }
 }
