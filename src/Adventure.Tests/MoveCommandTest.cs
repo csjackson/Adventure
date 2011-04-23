846534976e93d16e64f0e8810509397ffe949b59
Christopher Jackson <csjackson@gmail.com>
2011-04-22 22:35:42 -0500

    Move Command brings new vistas in fail

---
+++ src/Adventure.Tests/MoveCommandTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using Adventure.Data;
+using Adventure.Commands;
+
+namespace Adventure.Tests
+{
+    [TestClass]
+    public class MoveCommandTest : BaseTest
+    {
+        private IFormatter format;
+        private MoveCommand cmd;
+        [TestInitialize]
+        public void Before_Every_Test()
+        {
+            format = new Formatter(console, repository);
+            cmd = new MoveCommand(console, repository, player, aliasRepo, format);
+        }
+        [TestMethod]
+        public void IsValid_Should_Return_False_for_Invalid_String()
+        {
+            // Arrange
+            // Already created via TestInitialize
+
+            // Act
+            var result = cmd.IsValid("Stupid stuff");
+            // Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void isValid_Should_Return_True_for_Move_North()
+        {
+            // Arrange
+
+            // Act
+            var result = cmd.IsValid("move north");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void isValid_Should_Return_True_for_Go_North()
+        {
+            // Arrange
+
+            // Act
+            var result = cmd.IsValid("go north");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void isValid_Should_Return_True_for_North()
+        {
+            // Arrange
+
+            // Act
+            var result = cmd.IsValid("north");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Execute_Should_Update_Location_of_Player()
+        {
+            // Arrange
+
+            // Act
+            cmd.Execute("go north");
+
+            // Assert
+            Assert.AreEqual(dbPlayer.Location_Id, dbExit.Destination);
+        }
+        [TestMethod]
+      public void Execute_Should_display_Desc_of_New_Location_of_Player()
+      {
+            // Arrange
+
+            // Act
+          cmd.Execute("go north");
+
+           // Assert
+          console.AssertWasCalled(qq => qq.WriteLine(dbHallway.Description));
+      }
+    }
+}
---
+++ src/Adventure/Commands/MoveCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Adventure.Data;
+
+namespace Adventure.Commands
+{
+    public class MoveCommand : BaseCommand, ICommand
+    {
+             private IConsoleFacade console;
+        private IRepository<GameObject> repository;
+        private IPlayer player;
+        private IRepository<ExitAlias> aliases;
+        private IFormatter format;
+
+        public MoveCommand(IConsoleFacade console,
+            IRepository<GameObject> repository, IPlayer player, IRepository<ExitAlias> aliases, IFormatter format)
+        {
+            this.console = console;
+            this.repository = repository;
+            this.player = player;
+            this.aliases = aliases;
+            this.format = format;
+
+        }
+        public bool IsValid(string input)
+        {
+            bool placator = ((IsFirstWord(input, "move")) || (IsFirstWord(input, "go")));
+            return placator;
+        }
+          public void Execute(string input)
+        {
+            string Output = GetAllButFirstWord(input);
+              ExitAlias ChosenExit = null;
+            using (aliases)
+            {
+                 ChosenExit = aliases.AsQueryable().First(qq => 
+                     qq.Alais.Equals(Output, StringComparison.CurrentCultureIgnoreCase) );
+            }
+              using (repository)
+              {
+                  var pObj = repository.AsQueryable().First(qq => qq.GameObjectId == player.Id);
+                  pObj.Location_Id = ChosenExit.ExitId;
+                  var LocNow = repository.AsQueryable().FirstOrDefault(qq => qq.GameObjectId == ChosenExit.ExitId);
+                  format.Output(LocNow);
+              }
+            
+        }
+    }
+}
